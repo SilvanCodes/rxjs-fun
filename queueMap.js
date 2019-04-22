b145b7b5ed3ae8d35e5e7ec00a79037062b183f6
Silvan Büdenbender <s.buedenbender@gipfelstuermer-kommunikation.com>
2019-04-22 11:09:42 +0200

    small formating corrections and ajusted a few comments

--- queueMap.js
+++ queueMap.js
@@ -8,41 +8,41 @@ const queueMap = (innerObservable, minInnerEmissions = 1, exhaustQueue = true, c
     let sourceCompleted;
 
     const subFunc = {
-    next(innerValue) {
-      // emits inner observable to outer stream
-      observer.next(innerValue);
-      emissionCount += 1;
+      next(innerValue) {
+        // emits inner observable to outer stream
+        observer.next(innerValue);
+        emissionCount += 1;
 
-      // checks if innerObservable has satisfied minEmmissionCount
-      if (queue[0] && emissionCount >= minInnerEmissions) {
-        // switch to next observable in queue
-        emissionCount = 0;
-        activeInnerSubscription.unsubscribe();
-        activeInnerSubscription = innerObservable(queue.shift()).subscribe(subFunc);
-      } else if (emissionCount >= minInnerEmissions && completeAfterMinEmissions && sourceCompleted) {
-        // complete is configured to do so
-        activeInnerSubscription.unsubscribe();
-        observer.complete();
-      } else if (sourceCompleted && !exhaustQueue) {
-        // complete is configured to do so
-        activeInnerSubscription.unsubscribe();
-        observer.complete();
+        // checks if innerObservable has satisfied minEmmissionCount
+        if (queue[0] && emissionCount >= minInnerEmissions) {
+          // switch to next observable in queue
+          emissionCount = 0;
+          activeInnerSubscription.unsubscribe();
+          activeInnerSubscription = innerObservable(queue.shift()).subscribe(subFunc);
+        } else if (emissionCount >= minInnerEmissions && completeAfterMinEmissions && sourceCompleted) {
+          // complete observer if configured to do so
+          activeInnerSubscription.unsubscribe();
+          observer.complete();
+        } else if (sourceCompleted && !exhaustQueue) {
+          // complete observer if configured to do so
+          activeInnerSubscription.unsubscribe();
+          observer.complete();
+        }
+      },
+      // pass errors through
+      error(err) { observer.error(err); },
+      complete() {
+        // on completion, check if more in queue
+        if (queue[0]) {
+          // switch to next observable in queue
+          emissionCount = 0;
+          activeInnerSubscription = innerObservable(queue.shift()).subscribe(subFunc);
+        } else if (sourceCompleted) {
+          // complete observer if source completed
+          observer.complete();
+        }
       }
-    },
-    // pass errors through
-    error(err) { observer.error(err); },
-    complete() {
-      // on completion, check if more in queue
-      if (queue[0]) {
-        // switch to next observable in queue
-        emissionCount = 0;
-        activeInnerSubscription = innerObservable(queue.shift()).subscribe(subFunc);
-      } else if (sourceCompleted) {
-        // complete is configured to do so
-        observer.complete();
-      }
-    }
-    }
+    };
 
     return source.subscribe({
       next(outerValue) {
@@ -50,7 +50,7 @@ const queueMap = (innerObservable, minInnerEmissions = 1, exhaustQueue = true, c
         queue.push(outerValue);
 
         if (!activeInnerSubscription || activeInnerSubscription.closed) {
-          // initialize by subscribing to first in queue
+          // initialize or reactivate by subscribing to first in queue
           activeInnerSubscription = innerObservable(queue.shift()).subscribe(subFunc);
         }
       },

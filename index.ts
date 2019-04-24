d439b22c43ba88d15effdf75688b526cd79be2d4
Silvan Büdenbender <s.buedenbender@gipfelstuermer-kommunikation.com>
2019-04-24 10:27:37 +0200

    removed unnessecary files

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { of } from 'rxjs';
-import { expand, take } from 'rxjs/operators';
-
-const allInts = amount => of(1).pipe(
-  expand(val => of(val + 1)),
-  take(amount)
-);
-
-allInts(10).subscribe(console.log);
